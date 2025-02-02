07a2f85ac498cb49a19ad7b2b3ef6a27cd74da2b
Jean Mi <d@onidra.net>
2025-02-02 18:51:12 +0100

    question4 + insertion de emprunt

--- remplissage.sql
+++ remplissage.sql
@@ -378,3 +378,16 @@ INSERT INTO TRANSFERT (id_biblio_source, id_biblio_dest, id_edition, date_debut,
 ('BIB019', 'BIB014', 'EDT1', '2024-03-01', '2024-03-15', 'Transfert de roman classique', '-20'), -- Negative cost
 ('BIB007', 'BIB018', 'EDT2', '2024-04-01', '2024-04-15', 'Transfert dessai philosophique', '85'); --end date before start date: edge case problem
 
+INSERT INTO EMPRUNT (id_edition, id_abonne, date_emprunt, date_prevu, date_retour, etat_init, etat_retour)
+VALUES 
+ ('EDT1', 'AB001', '2025-01-01', '2025-02-01', '2025-01-20', 'Bon état', 'Bon état'), -- Example 1: Regular borrowing
+ ('EDT2', 'AB002', '2025-02-03', '2025-03-01', NULL, 'Neuf', NULL), -- Example 2: Book borrowed and not yet returned
+ ('EDT3', 'AB003', '2025-01-10', '2025-02-10', '2025-02-05', 'Bon état', 'Bon état'),
+ ('EDT3', 'AB004', '2025-02-20', '2025-03-20', NULL, 'Bon état', NULL), -- Example 3: Multiple borrowings by different users
+ ('EDT4', 'AB005', '2025-03-01', '2025-04-01', NULL, 'Neuf', NULL),-- Example 4: Future borrowings
+ ('EDT6', 'AB007', '2025-01-20', '2025-02-20', '2025-02-15', 'Neuf', 'Abîmé'), -- Example 6: Damaged book return
+ ('EDT7', 'AB008', '2025-02-05', '2025-02-10', '2025-02-08', 'Bon état', 'Bon état'), -- Example 7: Short borrow duration
+ ('EDT8', 'AB009', '2025-01-01', '2025-06-01', '2025-05-25', 'Neuf', 'Bon état'), -- Example 8: Long borrow duration
+ ('EDT9', 'AB010', '2025-01-15', '2025-02-15', '2025-02-05', 'Ancien', 'Ancien'),-- Example 9: Borrowing of a very old edition
+ ('EDT10', 'AB001', '2025-02-15', '2025-03-15', NULL, 'Bon état', NULL),
+ ('EDT2', 'AB001', '2025-01-01', '2025-02-01', '2025-02-02', 'Bon état', 'Bon état') -- Example 10: Returning late
