cdb2f09722cfc44824f7ee07e4b996e9a5eab55a
zaynab-nachabe <nachabezaynab5@gmail.com>
2025-02-05 08:37:41 +0100

    fonction last minute

---
+++ commandes/function.sql
@@ -0,0 +1,16 @@
+--fonction qui calcule le nombre total de livre emprunté par un abonné
+CREATE OR REPLACE FUNCTION total_books_borrowed(p_id_abonne VARCHAR(42))
+RETURNS INT AS $$
+DECLARE
+  total_borrowed INT;
+BEGIN
+  -- Calculate the total number of books borrowed by the subscriber
+  SELECT COUNT(*)
+  INTO total_borrowed
+  FROM EMPRUNT
+  WHERE id_abonne = p_id_abonne;
+
+  -- Return the total number of books borrowed
+  RETURN total_borrowed;
+END;
+$$ LANGUAGE plpgsql;
